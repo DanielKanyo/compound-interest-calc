27a9e7c00f376c3af8b8e459d5cec1f33617a83d
DanielKanyo <danielkanyog@gmail.com>
2024-09-27 14:27:43 +0200

    Css fixes, refactorings

--- src/Components/ChartArea.tsx
+++ src/Components/ChartArea.tsx
@@ -1,6 +1,6 @@
 import { AreaChart } from "@mantine/charts";
 import "@mantine/charts/styles.css";
-import { Avatar, Card, Flex, Group, NumberFormatter, Text } from "@mantine/core";
+import { Avatar, Card, Flex, Group, Text } from "@mantine/core";
 import { IconPigMoney, IconTrendingUp } from "@tabler/icons-react";
 
 import { Yearly } from "../Types/Yearly";
@@ -10,6 +10,31 @@ type ChartAreaProps = {
     goal: number | string;
 };
 
+type StatCardProps = {
+    title: string;
+    value: number;
+    icon: React.ElementType;
+    color: string;
+};
+
+const StatCard = ({ title, value, icon: Icon, color }: StatCardProps) => (
+    <Card shadow="sm" pt={20} pb={12} px="lg" radius="md" bg={color}>
+        <Flex gap="lg">
+            <Flex align="center">
+                <Avatar variant="filled" radius="md" size={55} mt={-10} color={`${color}.9`}>
+                    <Icon size="2rem" />
+                </Avatar>
+            </Flex>
+            <Flex direction="column" me={5}>
+                <Text c="gray.3">{title}</Text>
+                <Text fz={32} c="white" mt={-4}>
+                    {new Intl.NumberFormat("nb-NO", { maximumFractionDigits: 0 }).format(value)}
+                </Text>
+            </Flex>
+        </Flex>
+    </Card>
+);
+
 export const ChartArea = ({ yearly, goal }: ChartAreaProps) => {
     const totalSavings = yearly.length ? yearly[yearly.length - 1].value : 0;
     const totalContributions = yearly.length ? yearly[yearly.length - 1].contribution : 0;
@@ -17,39 +42,11 @@ export const ChartArea = ({ yearly, goal }: ChartAreaProps) => {
     return (
         <>
             <Group gap="md" mb="xs">
-                <Card shadow="sm" pt={20} pb={12} px="lg" radius="md" bg="teal">
-                    <Flex gap="lg">
-                        <Flex align="center">
-                            <Avatar variant="filled" radius="md" size={60} mt={-10} color="teal.9">
-                                <IconTrendingUp size="2rem" />
-                            </Avatar>
-                        </Flex>
-                        <Flex direction="column" me={5}>
-                            <Text c="gray.3">Total Savings</Text>
-                            <Text fz={36} c="white" mt={-4}>
-                                <NumberFormatter value={totalSavings} thousandSeparator=" " decimalScale={0} />
-                            </Text>
-                        </Flex>
-                    </Flex>
-                </Card>
-                <Card shadow="sm" pt={20} pb={12} px="lg" radius="md" bg="blue">
-                    <Flex gap="lg">
-                        <Flex align="center">
-                            <Avatar variant="filled" radius="md" size={60} mt={-10} color="blue.9">
-                                <IconPigMoney size="2rem" />
-                            </Avatar>
-                        </Flex>
-                        <Flex direction="column" me={5}>
-                            <Text c="gray.3">Total Contributions</Text>
-                            <Text fz={36} c="white" mt={-4}>
-                                <NumberFormatter value={totalContributions} thousandSeparator=" " decimalScale={0} />
-                            </Text>
-                        </Flex>
-                    </Flex>
-                </Card>
+                <StatCard title="Total Savings" value={totalSavings} icon={IconTrendingUp} color="teal" />
+                <StatCard title="Total Contributions" value={totalContributions} icon={IconPigMoney} color="blue" />
             </Group>
             <AreaChart
-                h="calc(100vh - 271px)"
+                h="calc(100vh - 264px)"
                 data={yearly}
                 withLegend
                 tickLine="xy"
